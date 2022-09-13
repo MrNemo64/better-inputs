f0e0b7a05958455b925e7b33853de248bf680aec
Lauriichan <laura.endress@syntaxphoenix.com>
2022-09-13 16:57:13 +0200

    Implement PlayerListener and base VersionHandler

---
+++ bukkit-parent/bukkit-api/src/main/java/me/nemo_64/betterinputs/nms/PlayerListener.java
@@ -0,0 +1,27 @@
+package me.nemo_64.betterinputs.nms;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public final class PlayerListener implements Listener {
+
+    private final VersionHandler handler;
+
+    public PlayerListener(final VersionHandler handler) {
+        this.handler = handler;
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onJoin(PlayerJoinEvent event) {
+        handler.join(event.getPlayer());
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onQuit(PlayerQuitEvent event) {
+        handler.quit(event.getPlayer());
+    }
+
+}
--- bukkit-parent/bukkit-api/src/main/java/me/nemo_64/betterinputs/nms/VersionHandler.java
+++ bukkit-parent/bukkit-api/src/main/java/me/nemo_64/betterinputs/nms/VersionHandler.java
@@ -1,9 +1,79 @@
 package me.nemo_64.betterinputs.nms;
 
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
 
 public abstract class VersionHandler {
 
-    public abstract PlayerAdapter getPlayer(UUID playerId);
+    protected final Plugin plugin;
+    protected final PlayerListener playerListener = new PlayerListener(this);
+
+    protected final ConcurrentHashMap<UUID, PlayerAdapter> players = new ConcurrentHashMap<>();
+
+    public VersionHandler(final Plugin plugin) {
+        this.plugin = plugin;
+    }
+
+    public final Plugin getPlugin() {
+        return plugin;
+    }
+
+    public final void enable() {
+        PluginManager pluginManager = Bukkit.getPluginManager();
+        onEnable(pluginManager);
+        pluginManager.registerEvents(playerListener, plugin);
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            join(player);
+        }
+    }
+
+    public final void disable() {
+        onDisable();
+        HandlerList.unregisterAll(playerListener);
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            quit(player);
+        }
+    }
+
+    protected void onEnable(final PluginManager pluginManager) {}
+
+    protected void onDisable() {}
+
+    public final PlayerAdapter getPlayer(UUID playerId) {
+        if (players.containsKey(playerId)) {
+            return players.get(playerId);
+        }
+        Player player = Bukkit.getPlayer(playerId);
+        if (player == null) {
+            return null;
+        }
+        PlayerAdapter adapter = createAdapter(player);
+        players.put(playerId, adapter);
+        return adapter;
+    }
+
+    final void join(Player player) {
+        if (players.containsKey(player.getUniqueId())) {
+            return;
+        }
+        createAdapter(player);
+    }
+
+    final void quit(Player player) {
+        if (!players.containsKey(player.getUniqueId())) {
+            return;
+        }
+        terminateAdapter(players.remove(player.getUniqueId()));
+    }
+
+    protected abstract PlayerAdapter createAdapter(Player player);
+
+    protected abstract void terminateAdapter(PlayerAdapter adapter);
 
 }
